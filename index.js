9ec55b919fabe0bcbb2b5113d6cce5957222ca0f
Mads Horsted Brunebjerg <mads81i7@edu.easj.dk>
2022-11-23 13:50:10 +0100

    +

--- index.js
+++ index.js
@@ -22,5 +22,15 @@ $(() => {
       },
       columns: ['CompanyName', 'City', 'State', 'Phone', 'Fax'],
       showBorders: true,
-    });
+    })
+    const popupContentTemplate = function () {
+      return $('<div>').append(
+        $(`<p>Full Name: <span>${employee.FirstName}</span>
+                           <span>${employee.LastName}</span></p>`),
+        $(`<p>Birth Date: <span>${employee.BirthDate}</span></p>`),
+        $(`<p>Address: <span>${employee.Address}</span></p>`),
+        $(`<p>Hire Date: <span>${employee.HireDate}</span></p>`),
+        $(`<p>Position: <span>${employee.Position}</span></p>`),
+      );
+    };
   });
