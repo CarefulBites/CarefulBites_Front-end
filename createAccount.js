dbb807897bfbe357e6fa8c98251edb82ad37bd21
Mads Horsted Brunebjerg <mads81i7@edu.easj.dk>
2022-11-28 13:08:34 +0100

    Added nav and footer to create account page.

--- createAccount.js
+++ createAccount.js
@@ -98,5 +98,25 @@ $(() => {
 
     e.preventDefault();
   });
+
+  $("#popup-button").dxButton({
+    styling: 'contained',
+    icon: 'user',
+    text: "Log In",
+    onClick: () => {
+        popup.show();
+    }
+  });
+  $("#theme-button").dxButton({
+    text: "change theme",
+    styling: 'contained',
+    onClick: () => {
+      if (DevExpress.ui.themes.current() == "material.blue.dark") {
+        DevExpress.ui.themes.current("material.blue.light");
+      } else {
+        DevExpress.ui.themes.current("material.blue.dark");
+      }
+    }
+  });
 });
 
